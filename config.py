094df07da1e42d746182fbabcba038fbaea44248
Ivan Kargapolov <i.kargapolov@g.nsu.ru>
2023-07-07 17:13:12 +0700

    A test with only one ion

---
+++ config.py
@@ -0,0 +1,18 @@
+
+config = {
+    'geometry': '3d',
+    'processing-unit-type': 'gpu',
+    'window-width-step-size': 0.05,
+    'window-width': 5,
+
+    'window-length': 20,
+    'xi-step': 0.05,
+
+    'time-limit': 1e-10,
+    'time-step': 1e-10,
+
+    'plasma-particles-per-cell': 1,
+
+    # This option disables the noise filter:
+    'enable-noise-filter': False
+}
